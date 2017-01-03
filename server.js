18788f1a78aacc60b4e473e48ad52b681c2fe7d5
Ryan Chacon <ryan.chacon103@yahoo.com>
2017-01-02 20:44:19 -0800

    added SASS capability

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import config from './config';
 import apiRouter from './api/index';
 import express from 'express';
+import sassMiddleware from 'node-sass-middleware';
+import path from 'path';
 
 const server = express();
+
+server.use(sassMiddleware({ //sassMiddleware is a function
+	src: path.join(__dirname, 'sass'), //where to pull the sass files - src directory
+	dest: path.join(__dirname, 'public') //where to put the sass - direct to write css
+}));
 
 server.set('view engine', 'ejs');
 
